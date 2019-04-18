280429abcfd569507842ee2b1e851415e1c9241b
rwadada <wadada.knct@gmail.com>
2019-04-18 22:50:39 +0900

    [update] add navigation

--- app/src/main/java/com/example/qiitaclient2/MainActivity.kt
+++ app/src/main/java/com/example/qiitaclient2/MainActivity.kt
@@ -4,17 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProviders
 import com.example.qiitaclient2.databinding.MainActivityBinding
-import com.example.qiitaclient2.splash.SplashFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
 
         val binding: MainActivityBinding =
             DataBindingUtil.setContentView(this, R.layout.main_activity) as MainActivityBinding
@@ -24,10 +20,6 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
 
-        val fragmentManager: FragmentManager = supportFragmentManager
-        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment, SplashFragment())
-            .commit()
     }
 
 }
--- app/src/main/java/com/example/qiitaclient2/splash/SplashFragment.kt
+++ app/src/main/java/com/example/qiitaclient2/splash/SplashFragment.kt
@@ -4,21 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.qiitaclient2.R
 
 class SplashFragment : Fragment() {
-    companion object {
-        fun newInstance() = SplashFragment()
-    }
-    private lateinit var viewModel: SplashViewModel
+
+    private lateinit var titleView: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_splash, container, false)
+        titleView = view.findViewById(R.id.title)
+
+        val thread = Thread(Runnable {
+            Thread.sleep(2000)
+            startAnimation(view)
+        })
+        thread.start()
+
+        return view
+    }
+
+    private fun startAnimation(view: View) {
+        val animation: Animation = AnimationUtils.loadAnimation(activity?.applicationContext, R.anim.animation_splash)
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                // NOP
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                titleView.visibility = View.GONE
+                val thread = Thread(Runnable {
+                    Thread.sleep(1000)
+                    Navigation.findNavController(view).navigate(R.id.action_first_to_second)
+                })
+                thread.start()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+                // NOP
+            }
+        })
+        titleView.startAnimation(animation)
     }
 
 
